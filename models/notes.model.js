e40c7e08df1fd4fbdbd7e0c272ec4c6eb84f5576
XDEV200 <xdev200200@gmail.com>
2021-06-23 11:23:16 +0530

    add: notes route

---
+++ models/notes.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const NotesSchema = new mongoose.Schema({
+  uid:String,
+  title: String,
+  note:String,
+  isArchive:{type:Boolean,default:false},
+  isDelete:{type:Boolean,default:false},
+  isPinned:{type:Boolean,default:false}
+},{timestamps: true});
+
+const Notes = mongoose.model("Notes",NotesSchema)
+module.exports = {Notes}
+
+
---
+++ routes/notes.router.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const { Notes } = require("../models/notes.model");
+const { check, validationResult } = require("express-validator")
+
+router
+  .route("/")
+
+  .get(async (req, res) => {
+    try {
+      const data = await Notes.find({});
+      res.json({ success: true, data });
+    } catch (err) {
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "Unable to load Notes",
+          errorMessage: err.message,
+        });
+    }
+  })
+
+  .post(async (req, res) => {
+   
+    try {
+      const note = req.body;
+      const NewNote = new Notes(note);
+      const savedNote = await NewNote.save();
+      res.json({ success: true, note: savedNote });
+    } catch (err) {
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "unable to add Note",
+          errorMessage: err.message,
+        });
+    }
+  });
+
+module.exports = router;
