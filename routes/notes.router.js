57f962782a695290da8481a747adb07503777237
XDEV200 <xdev200200@gmail.com>
2021-06-26 09:28:46 +0530

    add: trash route

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -7,20 +7,20 @@ const _ = require("lodash");
 router
   .route("/")
 
-  .get(async (req, res) => {
-    try {
-      const data = await Notes.find().sort({_id:-1});
-      res.json({ success: true, data });
-    } catch (err) {
-      res
-        .status(500)
-        .json({
-          success: false,
-          message: "Unable to load Notes",
-          errorMessage: err.message,
-        });
-    }
-  })
+  // .get(async (req, res) => {
+  //   try {
+  //     const data = await Notes.find().sort({_id:-1});
+  //     res.json({ success: true, data });
+  //   } catch (err) {
+  //     res
+  //       .status(500)
+  //       .json({
+  //         success: false,
+  //         message: "Unable to load Notes",
+  //         errorMessage: err.message,
+  //       });
+  //   }
+  // })
 
   .post(async (req, res) => {
    
@@ -40,6 +40,23 @@ router
     }
   });
 
+  router.route("/:uid")
+  .get(async (req, res) => {
+    try {
+      const uid = req.params.uid
+      const data = await Notes.find({uid:uid}).sort({_id:-1});
+      res.json({ success: true, data });
+    } catch (err) {
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "Unable to load Notes",
+          errorMessage: err.message,
+        });
+    }
+  })
+
   router
   .route("/change-color")
   .post(async(req,res)=>{
@@ -50,6 +67,31 @@ router
       await noteData.save();
     }catch(err){
       res.status(500).json({success:false,message:"unable to change the color",errorMessage:err.message})
+    }
+  })
+  router
+  .route("/archive")
+  .post(async(req,res)=>{
+    try{
+      const {id} = req.body
+      const note = await Notes.findOne({_id:id});
+      const noteData =_.extend(note, {isArchive:!note.isArchive,isDelete:false});
+      await noteData.save();
+    }catch(err){
+      res.status(500).json({success:false,message:"unable to Archive the note",errorMessage:err.message})
+    }
+  })
+
+  router
+  .route("/trash")
+  .post(async(req,res)=>{
+    try{
+      const {id} = req.body
+      const note = await Notes.findOne({_id:id});
+      const noteData =_.extend(note, {isDelete:!note.isDelete,isArchive:false});
+      await noteData.save();
+    }catch(err){
+      res.status(500).json({success:false,message:"unable to Trash the note",errorMessage:err.message})
     }
   })
 
